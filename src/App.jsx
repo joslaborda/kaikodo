e1ba0beafe3cd30564403c524e5283685784e92d
joslaborda <jlabord@gmail.com>
2026-08-24 22:24:52 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Toaster } from "@/components/ui/toaster"
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClientInstance } from '@/lib/query-client'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
+import { queryClientInstance, persistOptions } from '@/lib/query-client'
 import NavigationTracker from '@/lib/NavigationTracker'
 import { pagesConfig } from './pages.config'
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
@@ -216,13 +216,13 @@ function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <AuthProvider>
-        <QueryClientProvider client={queryClientInstance}>
+        <PersistQueryClientProvider client={queryClientInstance} persistOptions={persistOptions}>
           <Router>
             <NavigationTracker />
             <AuthenticatedApp />
           </Router>
           <Toaster />
-        </QueryClientProvider>
+        </PersistQueryClientProvider>
       </AuthProvider>
     </I18nextProvider>
   )
